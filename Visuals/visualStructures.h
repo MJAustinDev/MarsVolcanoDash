9d7dc7b70df7538ad997d039f08390078bd5ff5f
MJAustinDev <maustin2022@gmail.com>
2022-07-31 11:35:52 +0100

    shaded car details and updated release exe

--- Visuals/visualStructures.h
+++ Visuals/visualStructures.h
@@ -40,84 +40,85 @@ struct DrawShape{
 //Contains information to draw player car details
 struct CarDetails {
 
+    //coordinate points of car details relative to car body
     b2Vec2 points[19][4] = {
         //window
-        {b2Vec2(-0.1f,1.1f), b2Vec2(1.4f,1.1f), b2Vec2(1.05f,1.84f), b2Vec2(-0.1f,1.85f)},
+        {b2Vec2(-0.1f,1.1f), b2Vec2(1.4f,1.1f), b2Vec2(1.05f,1.84f), b2Vec2(-0.1f,1.85f)}, //bottom left, bottom right, top right, top left
 
         //front light
-        {b2Vec2(2.4,1.0), b2Vec2(2.6,0.7), b2Vec2(2.98,0.55), b2Vec2(2.96,0.9) },
+        {b2Vec2(2.4,1.0), b2Vec2(2.6,0.7), b2Vec2(2.98,0.55), b2Vec2(2.96,0.9)}, //top left, bottom left, bottom right, top right
 
         //back light
-        {b2Vec2(-2.92,1.0), b2Vec2(-2.97,0.5), b2Vec2(-2.75,0.7), b2Vec2(-2.70,1.0)},
+        {b2Vec2(-2.92,1.0), b2Vec2(-2.97,0.5), b2Vec2(-2.75,0.7), b2Vec2(-2.70,1.0)}, //top left, bottom left, bottom right, top right
 
         //door handle
-        {b2Vec2(0.0,0.9),b2Vec2(0.0,0.8),b2Vec2(0.3,0.8),b2Vec2(0.3,0.9)},
+        {b2Vec2(0.0,0.9),b2Vec2(0.0,0.8),b2Vec2(0.4,0.8),b2Vec2(0.5,0.9)}, //top left, bottom left, bottom right, top right
 
-        //door line
-        {b2Vec2(1.50f,0.9f), b2Vec2(1.45f,1.0f), b2Vec2(1.25,0.65) , b2Vec2(1.30,0.55)},
-        {b2Vec2(1.25,0.65) , b2Vec2(1.30,0.55), b2Vec2(0.92,0.50), b2Vec2(0.85,0.58)},
-        {b2Vec2(0.92,0.50), b2Vec2(0.85,0.58), b2Vec2(0.67,0.0), b2Vec2(0.75,0.0)},
+        //door line -- top ridge, mid ridge, bottom ridge
+        {b2Vec2(1.50f,0.9f), b2Vec2(1.45f,1.0f), b2Vec2(1.25,0.65) , b2Vec2(1.30,0.55)}, //forward right, forward left, backward left, backward right
+        {b2Vec2(1.25,0.65) , b2Vec2(1.30,0.55), b2Vec2(0.92,0.50), b2Vec2(0.85,0.58)}, //forward left, forward right, backward right, backward left
+        {b2Vec2(0.92,0.50), b2Vec2(0.85,0.58), b2Vec2(0.67,0.0), b2Vec2(0.75,0.0)}, //forward right, forward left, backward left, backward right
 
         //door back line
-        {b2Vec2(-0.3,1.85), b2Vec2(-0.375,1.85), b2Vec2(-0.375,0), b2Vec2(-0.3,0)},
+        {b2Vec2(-0.3,1.85), b2Vec2(-0.375,1.85), b2Vec2(-0.375,0), b2Vec2(-0.3,0)}, //top right, top left, bottom left, bottom right
 
-        //front wheel guard
-        {b2Vec2(2.15,0.60), b2Vec2(2.2,0.7), b2Vec2(1.5,0.7), b2Vec2(1.5,0.60)},
-        {b2Vec2(2.7,0.4), b2Vec2(2.65,0.33), b2Vec2(2.15,0.60), b2Vec2(2.2,0.7)},
-        {b2Vec2(2.85,0.2), b2Vec2(2.8,0.1), b2Vec2(2.65,0.33), b2Vec2(2.7,0.4)},
-        {b2Vec2(3.05f,0.1), b2Vec2(3.05f,0.2), b2Vec2(2.85,0.2), b2Vec2(2.8,0.1)},
+        //front wheel guard -- back guard, mid guard, front guard, front ridge
+        {b2Vec2(2.15,0.60), b2Vec2(2.2,0.7), b2Vec2(1.5,0.7), b2Vec2(1.5,0.60)}, //bottom right, top right, top left, bottom left
+        {b2Vec2(2.7,0.4), b2Vec2(2.65,0.33), b2Vec2(2.15,0.60), b2Vec2(2.2,0.7)}, //top right, bottom right, bottom left, top left
+        {b2Vec2(2.85,0.2), b2Vec2(2.8,0.1), b2Vec2(2.65,0.33), b2Vec2(2.7,0.4)}, //top right, bottom right, bottom left, top left
+        {b2Vec2(3.05f,0.1), b2Vec2(3.05f,0.2), b2Vec2(2.85,0.2), b2Vec2(2.8,0.1)}, //bottom right, top right, top left, bottom left
 
-        //rear wheel guard
-        {b2Vec2(-2.15,0.60), b2Vec2(-2.2,0.7), b2Vec2(-0.5,0.7), b2Vec2(-0.5,0.60)},
-        {b2Vec2(-2.7,0.4), b2Vec2(-2.65,0.33), b2Vec2(-2.15,0.60), b2Vec2(-2.2,0.7)},
-        {b2Vec2(-2.85,0.2), b2Vec2(-2.8,0.1), b2Vec2(-2.65,0.33), b2Vec2(-2.7,0.4)},
-        {b2Vec2(-3.05f,0.1), b2Vec2(-3.05f,0.2), b2Vec2(-2.85,0.2), b2Vec2(-2.8,0.1)},
+        //rear wheel guard -- front guard, mid guard, back guard, back ridge
+        {b2Vec2(-2.15,0.60), b2Vec2(-2.2,0.7), b2Vec2(-0.5,0.7), b2Vec2(-0.5,0.60)}, //bottom left, top left, top right, bottom right
+        {b2Vec2(-2.7,0.4), b2Vec2(-2.65,0.33), b2Vec2(-2.15,0.60), b2Vec2(-2.2,0.7)}, //top left, bottom left, bottom right, top right
+        {b2Vec2(-2.85,0.2), b2Vec2(-2.8,0.1), b2Vec2(-2.65,0.33), b2Vec2(-2.7,0.4)}, //top left, bottom left, bottom right, top right
+        {b2Vec2(-3.05f,0.1), b2Vec2(-3.05f,0.2), b2Vec2(-2.85,0.2), b2Vec2(-2.8,0.1)}, //bottom left, top left, top right, bottom right
 
-        //bumpers
-        {b2Vec2(-3.1f,-0.15f), b2Vec2(-3.15f,-0.4f), b2Vec2(-2.9,-0.4), b2Vec2(-2.8,-0.15)}, //rear bumper
-        {b2Vec2(-0.8f,-0.15f), b2Vec2(-0.7f,-0.4f), b2Vec2(0.7,-0.4), b2Vec2(0.8,-0.15)}, //mid bumper
-        {b2Vec2(3.1f,-0.15f), b2Vec2(3.15f,-0.4f), b2Vec2(2.9,-0.4), b2Vec2(2.8,-0.15)} //front bumper
+        //bumpers -- rear bumper, mid bumper, front bumper
+        {b2Vec2(-3.1f,-0.15f), b2Vec2(-3.15f,-0.4f), b2Vec2(-2.9,-0.4), b2Vec2(-2.8,-0.15)}, //top left, bottom left, bottom right, top right
+        {b2Vec2(-0.8f,-0.15f), b2Vec2(-0.7f,-0.4f), b2Vec2(0.7,-0.4), b2Vec2(0.8,-0.15)}, //top left, bottom left, bottom right, top right
+        {b2Vec2(3.1f,-0.15f), b2Vec2(3.15f,-0.4f), b2Vec2(2.9,-0.4), b2Vec2(2.8,-0.15)} //top right, bottom right, bottom left, top left
 
     };
 
-    //TODO -- PICK MAXIMUM SHADE VALUES FOR ALL CAR DETAILS -- window + bumpers are complete
+    //shade value of car details at corresponding coordinate points
     float colour [19][4] = {
         //window
-        {0.65f,0.85f,1.0f,0.85f},
+        {0.65f,0.85f,1.0f,0.85f}, //bottom left, bottom right, top right, top left
 
         //front light
-        {1.0f, 1.0f, 1.0f, 1.0f},
+        {0.9f, 0.9f, 1.0f, 1.0f}, //top left, bottom left, bottom right, top right
 
         //back light
-        {1.0f, 1.0f, 1.0f, 1.0f},
+        {1.0f, 1.0f, 0.9f, 0.9f}, //top left, bottom left, bottom right, top right
 
         //door handle
-        {1.0f, 1.0f, 1.0f, 1.0f},
+        {0.9f, 0.85f, 0.8f, 1.0f}, //top left, bottom left, bottom right, top right
 
-        //door line
-        {1.0f, 1.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f, 1.0f},
+        //door line -- top ridge, mid ridge, bottom ridge
+        {0.88f, 1.0f, 1.0f, 0.58f}, //forward right, forward left, backward left, backward right
+        {1.0f, 0.58f, 0.58f, 1.0f}, //forward left, forward right, backward right, backward left
+        {0.58f, 1.0f, 1.0f, 0.58f}, //forward right, forward left, backward left, backward right
 
         //door back line
-        {1.0f, 1.0f, 1.0f, 1.0f},
+        {1.0f, 0.85f, 0.65f, 0.75f}, //top right, top left, bottom left, bottom right
 
-        //front wheel guard
-        {1.0f, 1.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f, 1.0f},
+        //front wheel guard -- back guard, mid guard, front guard, front ridge
+        {0.58f, 1.0f, 1.0f, 0.68f}, //bottom right, top right, top left, bottom left
+        {1.0f, 0.58f, 0.58f, 1.0f}, //top right, bottom right, bottom left, top left
+        {1.0f, 0.58f, 0.58f, 1.0f}, //top right, bottom right, bottom left, top left
+        {0.78f, 1.0f, 1.0f, 0.58f}, //bottom right, top right, top left, bottom left
 
-        //rear wheel guard
-        {1.0f, 1.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f, 1.0f},
-        {1.0f, 1.0f, 1.0f, 1.0f},
+        //rear wheel guard -- front guard, mid guard, back guard, back ridge
+        {0.58f, 1.0f, 1.0f, 0.68f}, //bottom left, top left, top right, bottom right
+        {1.0f, 0.58f, 0.58f, 1.0f}, //top left, bottom left, bottom right, top right
+        {1.0f, 0.58f, 0.58f, 1.0f}, //top left, bottom left, bottom right, top right
+        {0.78f, 1.0f, 1.0f, 0.58f}, //bottom left, top left, top right, bottom right
 
-        //bumpers
-        {1.0f,1.0f,0.52f,1.0f}, //rear bumper
-        {1.0f,0.52f,0.52f,1.0f}, //mid bumper
-        {1.0f,1.0f,0.52f,1.0f} //front bumper
+        //bumpers -- rear bumper, mid bumper, front bumper
+        {1.0f,1.0f,0.52f,1.0f}, //top left, bottom left, bottom right, top right
+        {1.0f,0.52f,0.52f,1.0f}, //top left, bottom left, bottom right, top right
+        {1.0f,1.0f,0.52f,1.0f} //top right, bottom right, bottom left, top left
     };
 
 };
